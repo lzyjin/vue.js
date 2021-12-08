23c4449b1acf84fc9a7f068d5309d3c6292abed7
lzyjin <1247y.j@gmail.com>
2021-12-08 23:01:31 +0900

    computed

--- vue-webgame/04.response-time-check/response-check.vue
+++ vue-webgame/04.response-time-check/response-check.vue
@@ -1,12 +1,24 @@
 <template>
+  <!-- 가장 바깥의 template태그 바로 아래에 template을 쓸 수 없다. 그대신 그 아래부터는 몇번이고 사용 가능. -->
   <div>
-    <div id="screen" v-bind:class="state" @click="onClickScreen">{{message}}</div>
     <!-- v-bind:class="속성명"를 :class=""로 축약할 수 있다 -->
     <!-- <div id="screen" :class="state">{{message}}</div> -->
-    <div>
-      <div>평균 시간: {{  result.reduce( (a, c) => a + c, 0 ) / result.length || 0 }}ms</div>
+    <div id="screen" v-bind:class="state" @click="onClickScreen">{{message}}</div>
+
+    <!-- v-show="" 따옴표안에 들어있는것이 true이면 show(보여주고), falsy이면 숨긴다(태그는 존재하고, display:none) -->
+    <!-- v-if와의 차이점은 v-if를 사용하면 태그 자체가 존재하지 않음 -->
+    <!-- <div v-show="result.length"> -->
+    <!-- <div v-if="result.length"> -->
+    <!-- 이 태그는 그냥 평균시간과 리셋버튼을 감싸는 역할만 하기때문에 div로 묶기 싫다면 template으로 바꿔도 된다 -->
+    <!-- 그러면 #screen과 평균시간, 리셋버튼은 형제레벨이 된다(템플릿은 없는 태그가 된다) -->
+    <template v-if="result.length">
+
+      <!-- 계산은 화면에서 안하는 것이 좋다. vue의 computed를 이용하자 -->
+      <!-- <div>평균 시간: {{  result.reduce( (a, c) => a + c, 0 ) / result.length || 0 }}ms</div> -->
+      <div>평균 시간: {{ average }}ms</div>
       <button @click="onReset">리셋</button>
-    </div>
+
+    </template>
   </div>
 </template>
 
@@ -25,7 +37,18 @@
         // v-bind:class="state"
         message: '클릭해서 시작하세요.'
       }
-    }, 
+    },
+    // computed는 data를 가공해서 사용할 때 사용. computed를 통해 가공하면 캐싱이 된다. 
+    // 캐싱이 중요한 이유는? 
+    // 일반 데이터인 message가 바뀌면 화면 전체 (template)를 다시 그린다. 그러면 result를 계산하는 부분까지 다시 계산한다. 
+    // message만 바뀌었는데 평균시간까지 다시 계산하니까, 예를들어 더 복잡한 계산을 하는 식이라면 메세지만 바꿨는데 로딩하는데 10초 이상이 걸릴 수도 있는 것. 
+    // computed를 사용하면 message가 바뀌면 average에는 아무런 영향이 없다.
+    // 성능 최적화를 위해 꼭 챙겨야할 부분이다.
+    computed: {
+      average() {
+        return this.result.reduce( (a, c) => a + c, 0 ) / this.result.length || 0 ;
+      }
+    },
     methods: {
       onReset() {
         this.result = [];
---
+++ vue-webgame/05.rockScissorsPaper/RockScissorsPaper.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <!-- v-bind:class와 v-bind:style은 조금 독특하다. 값에 객체형식을 넣을 수 있다 -->
+    <!-- state가 true라서 state만 class가 될 수 있다.  -->
+    <!-- 자바스크립트에서 css속성을 사용할 때는 -을 빼기기호로 인식하기때문에 대문자로 바꿔서 카멜케이스로 만들어야한다 -->
+    <!-- <div id="computer" :class="{ state: true, hello: false }" v-bind:style="{ backgroundImage: '', fontSize: '14px' }"></div> -->
+
+    <!-- imgCoord라는 데이터만 사용하는 것이 아니라 문자열과 붙어있으므로 이런것을 computed로 만든다 => 캐싱효과 -->
+    <!-- <div id="computer" :style="{ background: `url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ${imgCoord} 0`}"></div> -->
+    <div id="computer" :style="{ computedStyleObject }"></div>
+    <div>
+      <button @click="onClickButton('바위')">바위</button>
+      <button @click="onClickButton('가위')">가위</button>
+      <button @click="onClickButton('보')">보</button>
+    </div>
+    <div>{{result}}</div>
+    <div>현재 {{score}}점</div>
+  </div>
+</template>
+
+<script>
+  const rspCoords = {
+    바위: '0',
+    가위: '-142px',
+    보: '-284px'
+  }
+  export default {
+    data() {
+      return {
+        imgCoord: rspCoords.바위,
+        result: '',
+        score: 0
+      }
+    },
+    computed: {
+      computedStyleObject() {
+        return {
+          background: `url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ${this.imgCoord} 0`
+        }
+      }
+    },
+    methods: {
+      onClickButton(choice) {
+
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  #computer {
+    width: 142px;
+    height: 200px;
+    background-position: 0 0;
+  }
+</style>
