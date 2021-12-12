d4f0b4243e7a9cf04b4cea9520181630a72e05e1
lzyjin <1247y.j@gmail.com>
2021-12-12 22:26:59 +0900

    vue

---
+++ vue-webgame/06.lotto/LottoBall.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div>당첨 숫자</div>
+    <lotto-ball v-for="ball in winBalls" v-bind:key="ball.idx"></lotto-ball>
+    <div>보너스</div>
+    <lotto-ball v-if="bonus"></lotto-ball>
+    <button v-if="redo">한번 더!</button>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+      return {
+        winBalls: [],
+        bonus: null,
+        redo: fales,
+      }
+    },
+    computed: {
+    },
+    methods: {
+    },
+    mounted() {
+    },
+    beforeDestroy() {
+    },
+    watch: {
+
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
